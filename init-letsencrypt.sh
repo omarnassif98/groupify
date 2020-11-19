7f5fa37eb8cf891e2abf6e6ed83734b9d367c97c
omarnassif98 <33793027+omarnassif98@users.noreply.github.com>
2020-11-19 09:47:29 -0500

    updated to use listen.omarnassif.me

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(crowdtuner.app)
+domains=(listen.omarnassif.me)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="omar.nassif98@gmail.com" # Adding a valid address is strongly recommended
